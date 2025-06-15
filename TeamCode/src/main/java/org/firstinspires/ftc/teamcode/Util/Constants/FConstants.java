3337c0bc36168986235e9f6f8d74f88ce426dabf
Daniel <cortezdaniel707@gmail.com>
2025-06-15 14:08:36 -0700

    tuned pedro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/FConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/FConstants.java
@@ -22,29 +22,29 @@ public class FConstants {
 
         FollowerConstants.mass = 12.33771;
 
-        FollowerConstants.xMovement = 71.274;
-        FollowerConstants.yMovement = 50.601;
+        FollowerConstants.xMovement = 51.0465;
+        FollowerConstants.yMovement = 37.7682;
 
-        FollowerConstants.forwardZeroPowerAcceleration = (-86.564 - 85.4255 - 90.9429 / 3);
-        FollowerConstants.lateralZeroPowerAcceleration = (-224.4798 - 197.7186 - 232.3678);
+        FollowerConstants.forwardZeroPowerAcceleration = (-37.1814);
+        FollowerConstants.lateralZeroPowerAcceleration = (-77.1355);
 
         FollowerConstants.translationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0);
-        FollowerConstants.useSecondaryTranslationalPID = false;
+        FollowerConstants.useSecondaryTranslationalPID = true;
         FollowerConstants.secondaryTranslationalPIDFCoefficients.setCoefficients(0.1,0,0.01,0); // Not being used, @see useSecondaryTranslationalPID
 
         FollowerConstants.headingPIDFCoefficients.setCoefficients(2,0,0.1,0);
-        FollowerConstants.useSecondaryHeadingPID = false;
+        FollowerConstants.useSecondaryHeadingPID = true;
         FollowerConstants.secondaryHeadingPIDFCoefficients.setCoefficients(2,0,0.1,0); // Not being used, @see useSecondaryHeadingPID
 
-        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0);
-        FollowerConstants.useSecondaryDrivePID = false;
-        FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.1,0,0,0.6,0); // Not being used, @see useSecondaryDrivePID
+        FollowerConstants.drivePIDFCoefficients.setCoefficients(0.05,0,0.003,0.6,0);
+        FollowerConstants.useSecondaryDrivePID = true;
+        FollowerConstants.secondaryDrivePIDFCoefficients.setCoefficients(0.05,0,0.003,0.6,0); // Not being used, @see useSecondaryDrivePID
 
         FollowerConstants.zeroPowerAccelerationMultiplier = 4;
         FollowerConstants.centripetalScaling = 0.0005;
 
-        FollowerConstants.pathEndTimeoutConstraint = 500;
-        FollowerConstants.pathEndTValueConstraint = 0.995;
+        FollowerConstants.pathEndTimeoutConstraint = 100;
+        FollowerConstants.pathEndTValueConstraint = 0.975;
         FollowerConstants.pathEndVelocityConstraint = 0.1;
         FollowerConstants.pathEndTranslationalConstraint = 0.1;
         FollowerConstants.pathEndHeadingConstraint = 0.007;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Constants/LConstants.java
@@ -6,14 +6,14 @@ import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 
 public class LConstants {
     static {
-        TwoWheelConstants.forwardTicksToInches = 0.0007;
-        TwoWheelConstants.strafeTicksToInches = 0.0007;
+        TwoWheelConstants.forwardTicksToInches = 0.0005;
+        TwoWheelConstants.strafeTicksToInches = 0.0005;
         TwoWheelConstants.forwardY = 2.7;
         TwoWheelConstants.strafeX = -2.25;
         TwoWheelConstants.forwardEncoder_HardwareMapName = "leftFront";
         TwoWheelConstants.strafeEncoder_HardwareMapName = "rightRear";
         TwoWheelConstants.forwardEncoderDirection = Encoder.FORWARD;
-        TwoWheelConstants.strafeEncoderDirection = Encoder.REVERSE;
+        TwoWheelConstants.strafeEncoderDirection = Encoder.REVERSE  ;
         TwoWheelConstants.IMU_HardwareMapName = "imu";
         TwoWheelConstants.IMU_Orientation = new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.RIGHT, RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD);
     }
